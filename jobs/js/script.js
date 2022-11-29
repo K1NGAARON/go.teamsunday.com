e10565a787074f7349c02d8a60b18271a75c3ceb
K1NGAARON <gerardaaron1997@gmail.com>
2022-11-29 14:09:33 +0100

    added img

--- jobs/js/script.js
+++ jobs/js/script.js
@@ -25,7 +25,10 @@ const jobsIMG = [
     './img/image-4.jpg',
     './img/image-5.jpg',
     './img/image-6.jpg',
-    './img/image-7.jpg'
+    './img/image-7.jpg',
+    './img/image-8.jpg',
+    './img/image-9.jpg',
+    './img/image-10.jpg'
 ];
 
 const jobsWrapper = document.querySelector("#jobs-section");
