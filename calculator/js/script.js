41675fb4e0cb75b08f4254bbe9a181fbbd257268
Bartel Samyn <bartel.samyn@teamsunday.com>
2022-12-02 17:07:27 +0100

    increased script version

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -76,27 +76,34 @@ const page = {
                 formElement.html('<h2>Thank you for your message!</h2>');
             }).catch((error) => {
                 if (error.response) {
-                    // The request was made and the server responded with a status code
-                    // that falls out of the range of 2xx
-                    console.log(error.response.data);
-                    console.log(error.response.status);
-                    console.log(error.response.headers);
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-                    // http.ClientRequest in node.js
-                    console.log(error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log('Error', error.message);
+                    // // The request was made and the server responded with a status code
+                    // // that falls out of the range of 2xx
+                    // console.log(error.response.data);
+                    // console.log(error.response.status);
+                    // console.log(error.response.headers);
+
+                    if (error.response.data.trim() !== '') {
+                        alert('There was an error sending your message. Please try again later.');
+                    }
+                // } else if (error.request) {
+                //     // The request was made but no response was received
+                //     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                //     // http.ClientRequest in node.js
+                //     console.log(error.request);
+                // } else {
+                //     // Something happened in setting up the request that triggered an Error
+                //     console.log('Error', error.message);
                 }
                 console.log(error.config);
-                alert('There was an error sending your message. Please try again later.');
+                self.revertForm();
             }).then(function () {
-                submitButton.prop('disabled', false);
-                emailInput.prop('disabled', true);
-                nameInputElement.prop('disabled', true);
+                self.revertForm();
             });
+        },
+        revertForm: function() {
+            submitButton.prop('disabled', false);
+            emailInput.prop('disabled', true);
+            nameInputElement.prop('disabled', true);
         },
         updateCalculations: function () {
             const slider = document.getElementById("myRange");
