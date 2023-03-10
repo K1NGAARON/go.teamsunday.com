32653280344818421a433f77413ed0f24db316e7
K1NGAARON <gerardaaron1997@gmail.com>
2023-03-10 18:02:22 +0100

    marketing webinar

---
+++ marketing-webinar/js/script.js
@@ -0,0 +1,29 @@
+function copySuccess() {
+    alert('URL copied successfully!');
+};
+
+$('#share').on('click', function(e) {
+    e.preventDefault();
+    var $temp = $("<input>");
+    var $url = 'https://go.teamsunday.com/hr-lunch-sessions/';
+
+    $("body").append($temp);
+    $temp.val($url).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    copySuccess();
+});
+
+$('#webinarLink').on('click', function(e) {
+    e.preventDefault();
+    var $temp = $("<input>");
+    var $url = 'meet.google.com/pwd-imnb-wpg';
+
+    $("body").append($temp);
+    $temp.val($url).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    copySuccess();
+});
